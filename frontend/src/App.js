3f2772900896211e6fbcefbcd624f98da63bad8a
AlexShonia <alex.shonia123@gmail.com>
2023-09-28 20:05:50 +0400

    add stats_about page and switching between

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,35 @@ import './App.css';
 import React, {useState} from 'react';
 
 
+function Stat_page() {
+  return(
+    <ul>
+      <li>yes</li>
+    </ul>
+  )
+}
+
+function Skill_page() {
+  return(
+    <ul>
+      <li>no</li>
+    </ul>
+  )
+}
+
+// let is_stat = true
+
 export default function App() {
+  const [is_stat, set_is_stat] = useState(true);
+
+  function handleClick(page) {
+    if (page === "skills"){
+      set_is_stat(false)
+    } else {
+      set_is_stat(true)
+    }
+  }
+
   return(
     <>
       <div className="header">
@@ -15,7 +43,13 @@ export default function App() {
             <button> Front </button>
             <img src="https://www.thesun.ie/wp-content/uploads/sites/3/2023/09/crop-9027665.jpg?w=620"></img>
           </div>
-          <div className="stats_about"></div>
+          <div className="stats_about">
+            <div className='app_header'>
+              <button onClick={() => handleClick("stats")}>Stats</button>
+              <button onClick={() => handleClick("skills")}>Skills</button>
+            </div>
+            {is_stat ? <Stat_page/> : <Skill_page/> }
+          </div>
       </div>
     </>
   )
